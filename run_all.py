90b99346b3a09f783b1299ffe701863a9cc17532
fangtaoa <15026687761@163.com>
2021-01-14 10:07:42 +0800

    更新requirement.txt文件

--- run_all.py
+++ run_all.py
@@ -10,8 +10,9 @@ def main(env):
     report_dir = cf.get_node_value("runtime", "report_dir")
     now = time.strftime('%Y-%m-%d_%H_%M_%S', time.localtime(time.time()))
     report_name = os.path.join(report_dir, 'report_' + now + '.html')
-    pytest.main(["-q", "test_cases", "--env", f"{env}", "--html=" + report_name])
-    send_email(report_name)
+    # pytest.main(["-q", "test_cases", "--env", f"{env}", "--html=" + report_name])
+    pytest.main(["-q", "test_cases", "--env", f"{env}", f"--alluredir={report_dir}/allure_report"])
+    # send_email(report_name)
 
 
 if __name__ == '__main__':
